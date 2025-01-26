4a7c94967ebf7f629beb800a34daf2a088bcfe1e
Vo Van Nghia <git@vnghia.com>
2025-01-26 08:22:07 +0100

    docker: add bridge network

---
+++ homelab-docker/src/homelab_docker/network/__init__.py
@@ -0,0 +1,20 @@
+import pulumi_docker as docker
+from pydantic import BaseModel
+
+
+class Bridge(BaseModel):
+    name: str
+    ipv6: bool = True
+    labels: dict[str, str] = {}
+
+    def build_resource(self) -> docker.Network:
+        return docker.Network(
+            resource_name=self.name,
+            name=self.name,
+            driver="bridge",
+            ipv6=self.ipv6,
+            labels=[
+                docker.NetworkLabelArgs(label=k, value=v)
+                for k, v in self.labels.items()
+            ],
+        )
