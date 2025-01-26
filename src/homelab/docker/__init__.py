20a275966ea3c245136082f10af2a447cfd2d6df
Vo Van Nghia <git@vnghia.com>
2025-01-26 21:22:28 +0100

    homelab/docker: rename container to service

--- src/homelab/docker/__init__.py
+++ src/homelab/docker/__init__.py
@@ -1,8 +1,8 @@
 from pulumi import ComponentResource, ResourceOptions
 
-from homelab.docker.container import Container
 from homelab.docker.image import Image
 from homelab.docker.network import Network
+from homelab.docker.service import Service
 from homelab.docker.volume import Volume
 
 
@@ -16,6 +16,6 @@ class Docker(ComponentResource):
         self.network = Network(opts=self.child_opts)
         self.image = Image(opts=self.child_opts)
         self.volume = Volume(opts=self.child_opts)
-        self.container = Container(opts=self.child_opts)
+        self.service = Service(opts=self.child_opts)
 
         self.register_outputs({})
--- src/homelab/docker/service/__init__.py
+++ src/homelab/docker/service/__init__.py
@@ -1,9 +1,15 @@
 from pulumi import ComponentResource, ResourceOptions
 
+from homelab.docker.service.tailscale import Tailscale
 
-class Tailscale(ComponentResource):
-    RESOURCE_NAME = "tailscale"
+
+class Service(ComponentResource):
+    RESOURCE_NAME = "service"
 
     def __init__(self, opts: ResourceOptions | None) -> None:
         super().__init__(self.RESOURCE_NAME, self.RESOURCE_NAME, None, opts)
         self.child_opts = ResourceOptions(parent=self)
+
+        self.tailscale = Tailscale(opts=self.child_opts)
+
+        self.register_outputs({})
