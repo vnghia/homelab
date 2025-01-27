c1ba61ffcf4d56e4a742814b09892e9bbbaa2271
Vo Van Nghia <git@vnghia.com>
2025-01-27 17:46:45 +0100

    homelab/docker: add a base service

---
+++ src/homelab/docker/service/base.py
@@ -0,0 +1,46 @@
+import pulumi
+import pulumi_docker as docker
+from pulumi import ComponentResource, Input, Output, ResourceOptions
+
+from homelab import config
+from homelab.docker.image import Image
+from homelab.docker.volume import Volume
+
+
+class Base(ComponentResource):
+    def __init__(
+        self,
+        image: Image,
+        volume: Volume,
+        resource_name: str,
+        opts: ResourceOptions | None,
+    ) -> None:
+        self.image = image
+        self.volume = volume
+        self.resource_name = resource_name
+        super().__init__(resource_name, resource_name, None, opts)
+        self.child_opts = ResourceOptions(parent=self)
+
+    def build_containers(
+        self, envs: dict[str, dict[str, Input[str]]] = {}
+    ) -> dict[str, docker.Container]:
+        self.containers = {
+            name: model.build_resource(
+                self.resource_name,
+                images=self.image.remotes,
+                volumes=self.volume.volumes,
+                opts=self.child_opts,
+                envs=envs.get(name, {}),
+            )
+            for name, model in config.docker.services[
+                self.resource_name
+            ].containers.items()
+        }
+
+        for name, container in self.containers.items():
+            pulumi.export(f"container-{name}", container.name)
+
+        return self.containers
+
+    def container_outputs(self) -> dict[str, Output[str]]:
+        return {name: container.name for name, container in self.containers.items()}
--- src/homelab/docker/service/tailscale.py
+++ src/homelab/docker/service/tailscale.py
@@ -1,28 +1,38 @@
-from pulumi import ComponentResource, ResourceOptions
+import pulumi_tailscale as tailscale
+from pulumi import ResourceOptions
 
-from homelab import config
 from homelab.docker.image import Image
+from homelab.docker.service.base import Base
 from homelab.docker.volume import Volume
 
 
-class Tailscale(ComponentResource):
+class Tailscale(Base):
     RESOURCE_NAME = "tailscale"
 
     def __init__(
         self, image: Image, volume: Volume, opts: ResourceOptions | None
     ) -> None:
-        super().__init__(self.RESOURCE_NAME, self.RESOURCE_NAME, None, opts)
-        self.child_opts = ResourceOptions(parent=self)
-
-        self.container = (
-            config.docker.services[self.RESOURCE_NAME]
-            .containers[self.RESOURCE_NAME]
-            .build_resource(
-                self.RESOURCE_NAME,
-                images=image.remotes,
-                volumes=volume.volumes,
-                opts=self.child_opts,
-            )
+        super().__init__(
+            image=image, volume=volume, resource_name=self.RESOURCE_NAME, opts=opts
         )
 
-        self.register_outputs({})
+        self.build_containers(
+            envs={
+                self.resource_name: {
+                    "TS_AUTHKEY": self.build_authkey().key,
+                }
+            }
+        )
+
+        self.register_outputs(self.container_outputs())
+
+    def build_authkey(self) -> tailscale.TailnetKey:
+        self.authkey = tailscale.TailnetKey(
+            "tailscale",
+            opts=self.child_opts,
+            ephemeral=False,
+            expiry=5 * 60,
+            preauthorized=True,
+            reusable=False,
+        )
+        return self.authkey
