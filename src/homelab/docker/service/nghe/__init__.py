dd07fc581eaa7203a544971160ec1ca2043bd3c8
Vo Van Nghia <git@vnghia.com>
2025-02-06 20:11:39 +0100

    refactor(docker/nghe): add traefik file

--- src/homelab/docker/service/nghe/__init__.py
+++ src/homelab/docker/service/nghe/__init__.py
@@ -1,16 +1,22 @@
 import binascii
 
 import pulumi_random as random
-from homelab_docker.model.container.model import ContainerModelGlobalArgs
+from homelab_docker.model.container.model import (
+    ContainerModelBuildArgs,
+    ContainerModelGlobalArgs,
+)
 from homelab_docker.model.service import ServiceModel
 from homelab_docker.resource.service import ServiceResourceBase
 from pulumi import ResourceOptions
 
 from homelab.docker.service.nghe.config import NgheConfig
+from homelab.docker.service.traefik.config.dynamic.http import TraefikHttpDynamicConfig
 from homelab.docker.service.traefik.config.static import TraefikStaticConfig
 
 
 class NgheService(ServiceResourceBase[NgheConfig]):
+    KEY_LENGTH = 16
+
     def __init__(
         self,
         model: ServiceModel[NgheConfig],
@@ -23,13 +29,37 @@ class NgheService(ServiceResourceBase[NgheConfig]):
             model, opts=opts, container_model_global_args=container_model_global_args
         )
 
-        self.build_containers(options={})
+        self.key = random.RandomPassword(
+            "key", opts=self.child_opts, length=self.KEY_LENGTH, special=False
+        ).result
+        self.build_containers(
+            options={
+                None: ContainerModelBuildArgs(
+                    envs={
+                        "NGHE_DATABASE__KEY": self.key.apply(
+                            lambda x: binascii.hexlify(x.encode()).decode("ascii")
+                        ),
+                        "NGHE_INTEGRATION__SPOTIFY__ID": self.model.config.spotify.id,
+                        "NGHE_INTEGRATION__SPOTIFY__SECRET": self.model.config.spotify.secret,
+                        "NGHE_INTEGRATION__LASTFM__KEY": self.model.config.lastfm.key,
+                        "NGHE_S3__ENABLE": "true",
+                    }
+                )
+            }
+        )
 
-        # self.service_config = self.config().config(Config)
+        self.traefik = TraefikHttpDynamicConfig(
+            name=self.name(),
+            public=True,
+            service=int(self.model.container.envs["NGHE_SERVER__PORT"].to_str()),
+        ).build_resource(
+            "traefik",
+            opts=self.child_opts,
+            volume_resource=container_model_global_args.docker_resource.volume,
+            containers=self.CONTAINERS,
+            static_config=traefik_static_config,
+        )
 
-        # self.key = random.RandomPassword(
-        #     "key", opts=self.child_opts, length=self.KEY_LENGTH, special=False
-        # ).result
         # self.build_containers(
         #     options={
         #         None: BuildOption(
@@ -47,12 +77,4 @@ class NgheService(ServiceResourceBase[NgheConfig]):
         #             opts=ResourceOptions(depends_on=[self.postgres[None].container]),
         #         )
         #     }
-        # )
-
-        # self.traefik = HttpDynamic(
-        #     name=self.name(),
-        #     public=True,
-        #     service=int(self.config().container.envs["NGHE_SERVER__PORT"].to_str()),
-        # ).build_resource(
-        #     "traefik", resource=resource, traefik=traefik, opts=self.child_opts
         # )
