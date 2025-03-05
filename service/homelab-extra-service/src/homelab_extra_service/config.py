f654847257c1814d2d67ce1f741df143e06c55d7
Vo Van Nghia <git@vnghia.com>
2025-03-05 01:31:30 +0100

    feat(extra): add dagu service config to extra

--- service/homelab-extra-service/src/homelab_extra_service/config.py
+++ service/homelab-extra-service/src/homelab_extra_service/config.py
@@ -1,6 +1,7 @@
+from homelab_dagu_config import DaguServiceConfigBase
 from homelab_s3_config import S3ServiceConfigBase
 from homelab_traefik_config import TraefikServiceConfigBase
 
 
-class ExtraConfig(TraefikServiceConfigBase, S3ServiceConfigBase):
+class ExtraConfig(TraefikServiceConfigBase, S3ServiceConfigBase, DaguServiceConfigBase):
     pass
