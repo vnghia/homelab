cbc6e7599c58f38c1f30f3b9540b25c99dc00aa6
Vo Van Nghia <git@vnghia.com>
2025-03-24 08:29:47 +0100

    feat(secret): add keepass upsert

--- homelab-secret/src/homelab_secret/resource/keepass/__init__.py
+++ homelab-secret/src/homelab_secret/resource/keepass/__init__.py
@@ -3,6 +3,7 @@ from contextlib import contextmanager
 from typing import Any, Iterator, Self
 from uuid import UUID
 
+import pulumi
 from homelab_pydantic import HomelabBaseModel
 from pulumi.dynamic import (
     CreateResult,
@@ -27,9 +28,26 @@ class KeepassEntryProps(HomelabBaseModel):
     urls: list[HttpUrl]
     apps: list[str]
 
+    @classmethod
+    def from_entry(cls, entry: Entry) -> Self:
+        return cls.model_validate(
+            {
+                "username": entry.username,
+                "password": entry.password,
+                "hostname": entry.url,
+                "urls": [],
+                "apps": [],
+            }
+        )
+
+
+class KeepassProviderProps(HomelabBaseModel):
+    entries: dict[str, KeepassEntryProps]
+    entry_ids: dict[str, UUID] = {}
+
 
 class Keepass:
-    def __init__(self, filename: str) -> None:
+    def __init__(self, filename: str, props: KeepassProviderProps) -> None:
         self.keepass = PyKeePass(
             filename,
             password=os.environ.get("KEEPASS_PASSWORD"),
@@ -42,49 +60,72 @@ class Keepass:
         assert isinstance(group, Group)
         self.group = group
 
+        self.props = props
+
     def save(self) -> None:
         self.keepass.save()
 
-    def find_entry(self, title: str) -> KeepassEntryProps | None:
+    def find_entry(self, title: str) -> Entry | None:
+        entry_id = self.props.entry_ids.get(title)
+        if entry_id:
+            entry = self.keepass.find_entries(uuid=entry_id, first=True)
+            if entry:
+                assert isinstance(entry, Entry)
+                return entry
+            else:
+                pulumi.warn(
+                    "Could not find entry with title {} and id {}".format(
+                        title, entry_id
+                    )
+                )
+                self.props.entry_ids.pop(title)
+
         path = list(self.group.path) + [title]
         entry = self.keepass.find_entries(path=path)
         if entry:
             assert isinstance(entry, Entry)
-            return KeepassEntryProps.model_validate(
-                {
-                    "username": entry.username,
-                    "password": entry.password,
-                    "hostname": entry.url,
-                    "urls": [],
-                    "apps": [],
-                }
-            )
+            self.props.entry_ids[title] = entry.uuid
+            return entry
         else:
             return None
 
     def add_entry(self, title: str, props: KeepassEntryProps) -> None:
-        self.keepass.add_entry(
+        entry = self.keepass.add_entry(
             destination_group=self.group,
             title=title,
             username=props.username,
             password=props.password,
             url=str(props.hostname),
         )
+        self.props.entry_ids[title] = entry.uuid
+
+    def update_entry(self, entry: Entry, props: KeepassEntryProps) -> None:
+        entry.username = props.username
+        entry.password = props.password
+        entry.url = str(props.hostname)
+
+    def upsert_props(self) -> None:
+        for title, entry_props in self.props.entries.items():
+            entry = self.find_entry(title)
+            if not entry:
+                self.add_entry(title=title, props=entry_props)
+            elif KeepassEntryProps.from_entry(entry) != entry_props:
+                self.update_entry(entry, entry_props)
+        for entry in self.group.entries:
+            entry_id = self.props.entry_ids.get(str(entry.title))
+            if entry.uuid != entry_id:
+                self.keepass.delete_entry(entry)
 
     @classmethod
     @contextmanager
-    def open(cls) -> Iterator[Self]:
+    def open(cls, props: KeepassProviderProps) -> Iterator[Self]:
         filename = os.environ.get("KEEPASS_DATABASE")
         if filename:
-            keepass = cls(filename)
+            keepass = cls(filename, props)
             try:
                 yield keepass
             finally:
                 keepass.save()
-
-
-class KeepassProviderProps(HomelabBaseModel):
-    entries: dict[str, KeepassEntryProps]
 
 
 class KeepassProvider(ResourceProvider):
@@ -94,11 +135,8 @@ class KeepassProvider(ResourceProvider):
 
     def create(self, props: dict[str, Any]) -> CreateResult:
         keepass_props = KeepassProviderProps(**props)
-        with Keepass.open() as keepass:
-            for title, entry_props in keepass_props.entries.items():
-                entry = keepass.find_entry(title)
-                if not entry:
-                    keepass.add_entry(title=title, props=entry_props)
+        with Keepass.open(keepass_props) as keepass:
+            keepass.upsert_props()
         return CreateResult(
             id_=self.RESOURCE_ID, outs=keepass_props.model_dump(mode="json")
         )
