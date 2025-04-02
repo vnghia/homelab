2d2da85dcc70be89978b9165297e9c2f01591dcf
Vo Van Nghia <git@vnghia.com>
2025-04-02 16:21:49 +0200

    fix(kanidm): delete container after extract

--- service/homelab-kanidm-service/src/homelab_kanidm_service/client.py
+++ service/homelab-kanidm-service/src/homelab_kanidm_service/client.py
@@ -1,6 +1,7 @@
 import re
+from contextlib import contextmanager
 from pathlib import PosixPath
-from typing import ClassVar
+from typing import ClassVar, Iterator
 
 from homelab_docker.client import DockerClient
 from homelab_pydantic import AbsolutePath, HomelabBaseModel
@@ -23,44 +24,49 @@ class KanidmClient(HomelabBaseModel):
     image: str
     cache: str
 
+    @contextmanager
     def run(
         self, command: list[str] | str, environment: dict[str, str] | None = None
-    ) -> str:
-        return (
-            DockerClient()
-            .containers.run(
-                image=self.image,
-                command=command,
-                entrypoint=[],
-                environment={
-                    "KANIDM_URL": "https://{}:{}".format(self.host, self.port),
-                    "KANIDM_NAME": self.ACCOUNT,
-                    "KANIDM_SKIP_HOSTNAME_VERIFICATION": "true",
-                    "KANIDM_ACCEPT_INVALID_CERTS": "true",
-                }
-                | (environment or {}),
-                network=self.network,
-                remove=False,
-                volumes={
-                    self.cache: {"bind": self.CACHE_PATH.as_posix(), "mode": "rw"}
-                },
-            )
-            .decode()
+    ) -> Iterator[str]:
+        container = DockerClient().containers.run(
+            image=self.image,
+            command=command,
+            entrypoint=[],
+            detach=True,
+            environment={
+                "KANIDM_URL": "https://{}:{}".format(self.host, self.port),
+                "KANIDM_NAME": self.ACCOUNT,
+                "KANIDM_SKIP_HOSTNAME_VERIFICATION": "true",
+                "KANIDM_ACCEPT_INVALID_CERTS": "true",
+            }
+            | (environment or {}),
+            network=self.network,
+            remove=False,
+            volumes={self.cache: {"bind": self.CACHE_PATH.as_posix(), "mode": "rw"}},
         )
+        try:
+            response = container.wait()
+            if response["StatusCode"] != 0:
+                raise RuntimeError(response)
+            yield container.logs(stdout=True, stderr=False, stream=False).decode()
+        finally:
+            container.remove(force=True)
 
     def login(self, password: str) -> str:
-        result = self.run(
+        with self.run(
             "/bin/sh -c 'echo \"${{KANIDM_PASSWORD}}\\n\" | {} login'".format(
                 self.BINARY_PATH.as_posix()
             ),
             environment={"KANIDM_PASSWORD": password},
-        )
-        if "Login Success for {}@".format(self.ACCOUNT) not in result:
-            raise RuntimeError("Could not login to kanidm with log: {}".format(result))
-        return self.ACCOUNT
+        ) as result:
+            if "Login Success for {}@".format(self.ACCOUNT) not in result:
+                raise RuntimeError(
+                    "Could not login to kanidm with log: {}".format(result)
+                )
+            return self.ACCOUNT
 
     def extract_oauth_secret(self, system: str) -> str:
-        return self.run(
+        with self.run(
             [
                 self.BINARY_PATH.as_posix(),
                 "system",
@@ -68,4 +74,5 @@ class KanidmClient(HomelabBaseModel):
                 "show-basic-secret",
                 system,
             ]
-        ).replace("\n", "")
+        ) as result:
+            return result.replace("\n", "")
