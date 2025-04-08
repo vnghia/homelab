786b406164a428ac264e4b751a694d1a523fdf18
Vo Van Nghia <git@vnghia.com>
2025-04-08 15:58:31 +0200

    feat(alist): add wud config

--- homelab-docker/src/homelab_docker/model/container/wud.py
+++ homelab-docker/src/homelab_docker/model/container/wud.py
@@ -1,11 +1,16 @@
+from typing import ClassVar
+
 from homelab_pydantic import HomelabBaseModel
 
 
 class ContainerWudConfig(HomelabBaseModel):
+    SEMVER_PATTERN: ClassVar[str] = r"\d+\.\d+\.\d+"
+
     template: str | None = None
     include: str | None = None
     include_prefix: str = ""
     include_suffix: str = ""
+    transform: bool = False
 
     def build_labels(
         self,
@@ -14,12 +19,25 @@ class ContainerWudConfig(HomelabBaseModel):
         return (
             ({"wud.display.name": resource_name} if resource_name else {})
             | ({"wud.link.template": self.template} if self.template else {})
+            | (
+                {
+                    "wud.tag.transform": "^"
+                    + self.include_prefix
+                    + "("
+                    + self.SEMVER_PATTERN
+                    + ")"
+                    + self.include_suffix
+                    + "$ => $1"
+                }
+                if self.transform
+                else {}
+            )
             | {
                 "wud.tag.include": self.include
                 or (
                     "^"
                     + self.include_prefix
-                    + r"\d+\.\d+\.\d+"
+                    + self.SEMVER_PATTERN
                     + self.include_suffix
                     + "$"
                 ),
