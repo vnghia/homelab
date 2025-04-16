45b8eab6aa48419508179e7b4c51ca559c4dbca5
Vo Van Nghia <git@vnghia.com>
2025-04-16 08:32:42 +0200

    feat(apprise): move apprise to extra service

--- service/homelab-apprise-service/src/homelab_apprise_service/__init__.py
+++ service/homelab-apprise-service/src/homelab_apprise_service/__init__.py
@@ -1,12 +1,12 @@
 from homelab_docker.model.service import ServiceWithConfigModel
 from homelab_docker.resource import DockerResourceArgs
-from homelab_docker.resource.service import ServiceWithConfigResourceBase
+from homelab_extra_service import ExtraService
 from pulumi import ResourceOptions
 
 from .config import AppriseConfig
 
 
-class AppriseService(ServiceWithConfigResourceBase[AppriseConfig]):
+class AppriseService(ExtraService[AppriseConfig]):
     def __init__(
         self,
         model: ServiceWithConfigModel[AppriseConfig],
@@ -15,7 +15,4 @@ class AppriseService(ServiceWithConfigResourceBase[AppriseConfig]):
         docker_resource_args: DockerResourceArgs,
     ) -> None:
         super().__init__(model, opts=opts, docker_resource_args=docker_resource_args)
-
-        self.build_containers()
-
-        self.register_outputs({})
+        self.build()
