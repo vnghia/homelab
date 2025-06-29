5285079def37edb3d9acc977a0b35d9be8614ca7
Vo Van Nghia <git@vnghia.com>
2025-06-29 21:19:15 +0200

    fix(extract): circular dependencies

--- homelab-docker/src/homelab_docker/extract/kv.py
+++ homelab-docker/src/homelab_docker/extract/kv.py
@@ -1,11 +1,10 @@
 from __future__ import annotations
 
-import json
-import secrets
 import typing
-from typing import ClassVar, Never
+from typing import Never
 
 from homelab_extract.kv import GlobalExtractKvSource
+from homelab_extract.transform.string import ExtractTransformString
 from homelab_pydantic import HomelabRootModel
 from pulumi import Output
 
@@ -15,9 +14,6 @@ if typing.TYPE_CHECKING:
 
 
 class GlobalKvSourceExtractor(HomelabRootModel[GlobalExtractKvSource]):
-    EXTRACTOR_KEY: ClassVar[str] = "__kv_extractor"
-    EXTRACTOR_VALUE: ClassVar[str] = secrets.token_hex(16)
-
     def extract_str(
         self, main_service: ServiceResourceBase, model: ContainerModel | None
     ) -> Output[str]:
@@ -29,7 +25,11 @@ class GlobalKvSourceExtractor(HomelabRootModel[GlobalExtractKvSource]):
                 key: GlobalExtractor(value).extract_str(main_service, model)
                 for key, value in root.kv.items()
             }
-            | {self.EXTRACTOR_KEY: Output.from_input(self.EXTRACTOR_VALUE)}
+            | {
+                ExtractTransformString.JSON_KEY: Output.from_input(
+                    ExtractTransformString.JSON_VALUE
+                )
+            }
         )
 
     def extract_path(
@@ -41,16 +41,3 @@ class GlobalKvSourceExtractor(HomelabRootModel[GlobalExtractKvSource]):
         self, _main_service: ServiceResourceBase, _model: ContainerModel | None
     ) -> Never:
         raise TypeError("Can not extract volume path from kv source")
-
-    @classmethod
-    def parse(cls, value: str) -> dict[str, str] | str:
-        try:
-            data = json.loads(value)
-            if (
-                isinstance(data, dict)
-                and data.get(cls.EXTRACTOR_KEY) == cls.EXTRACTOR_VALUE
-            ):
-                return data
-            return value
-        except json.JSONDecodeError:
-            return value
--- homelab-extract/src/homelab_extract/transform/string.py
+++ homelab-extract/src/homelab_extract/transform/string.py
@@ -1,15 +1,19 @@
+import json
 import re
+import secrets
+from typing import ClassVar
 
 from homelab_pydantic import HomelabBaseModel
 
 
 class ExtractTransformString(HomelabBaseModel):
+    JSON_KEY: ClassVar[str] = "__json_key"
+    JSON_VALUE: ClassVar[str] = secrets.token_hex(16)
+
     capture: str | None = None
     template: str | None = None
 
     def transform(self, value: str) -> str:
-        from homelab_docker.extract.kv import GlobalKvSourceExtractor
-
         if self.capture:
             match = re.match(self.capture, value)
             if not match:
@@ -17,7 +21,7 @@ class ExtractTransformString(HomelabBaseModel):
             value = match[1]
 
         if self.template:
-            value_data = GlobalKvSourceExtractor.parse(value)
+            value_data = self.parse(value)
             format_args: dict[str, str] = {}
             if isinstance(value_data, dict):
                 format_args |= value_data
@@ -25,3 +29,13 @@ class ExtractTransformString(HomelabBaseModel):
                 format_args["value"] = value
             value = self.template.format(**format_args)
         return value
+
+    @classmethod
+    def parse(cls, value: str) -> dict[str, str] | str:
+        try:
+            data = json.loads(value)
+            if isinstance(data, dict) and data.get(cls.JSON_KEY) == cls.JSON_VALUE:
+                return data
+            return value
+        except json.JSONDecodeError:
+            return value
