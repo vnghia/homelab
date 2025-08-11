c82a90b362cb2b4fe73dd21d7b962f055d383907
Vo Van Nghia <git@vnghia.com>
2025-08-11 15:45:41 +0200

    chore: clean pydio and seafile service

--- service/homelab-pydio-service/src/homelab_pydio_service/config.py
+++
@@ -1,9 +0,0 @@
-from typing import Any
-
-from homelab_extra_service.config import ExtraConfig
-from homelab_extract.service import ServiceExtract
-
-
-class PydioConfig(ExtraConfig):
-    install: Any
-    install_path: ServiceExtract
--- service/homelab-seafile-service/src/homelab_seafile_service/config.py
+++
@@ -1,5 +0,0 @@
-from homelab_extra_service.config import ExtraConfig
-
-
-class SeafileConfig(ExtraConfig):
-    pass
