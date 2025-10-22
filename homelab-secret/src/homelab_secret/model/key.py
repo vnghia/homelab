6ce1e1eb0a16808a6a14d00cb3b88d750e203def
Vo Van Nghia <git@vnghia.com>
2025-10-22 13:05:32 +0200

    feat(secret/key): add ecdsa curve and rsa bits to tls private key

--- homelab-secret/src/homelab_secret/model/key.py
+++ homelab-secret/src/homelab_secret/model/key.py
@@ -1,14 +1,23 @@
 import pulumi_tls as tls
 from pulumi import ResourceOptions
+from pydantic import PositiveInt
 
 from . import SecretModel
 
 
 class SecretKeyModel(SecretModel):
     algorithm: str
+    ecdsa_curve: str | None = None
+    rsa_bits: PositiveInt | None = None
 
     def build_resource(
         self, resource_name: str, opts: ResourceOptions
     ) -> tls.PrivateKey:
         opts = self.opts(opts)
-        return tls.PrivateKey(resource_name, opts=opts, algorithm=self.algorithm)
+        return tls.PrivateKey(
+            resource_name,
+            opts=opts,
+            algorithm=self.algorithm,
+            ecdsa_curve=self.ecdsa_curve,
+            rsa_bits=self.rsa_bits,
+        )
