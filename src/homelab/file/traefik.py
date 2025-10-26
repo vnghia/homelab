7e89d2c6272340dce252fb155e3d8ebd6343c9fc
Vo Van Nghia <git@vnghia.com>
2025-10-26 15:39:51 +0100

    fix(file/traefik): always build traefik first

--- src/homelab/file/traefik.py
+++ src/homelab/file/traefik.py
@@ -1,4 +1,7 @@
-from homelab_docker.resource.service import ServiceWithConfigResourceBase
+from homelab_docker.resource.service import (
+    ServiceResourceBase,
+    ServiceWithConfigResourceBase,
+)
 from homelab_traefik_config import TraefikServiceConfigBase
 from homelab_traefik_service import TraefikService
 from homelab_traefik_service.config.service import TraefikServiceConfigBuilder
@@ -12,22 +15,32 @@ class TraefikFile(ComponentResource):
         super().__init__(self.RESOURCE_NAME, self.RESOURCE_NAME, None, opts=opts)
         self.child_opts = ResourceOptions(parent=self)
 
+        self.build_one(traefik_service, traefik_service)
         for service in traefik_service.extractor_args.services.values():
-            if (
-                isinstance(service, ServiceWithConfigResourceBase)
-                and isinstance(service.config, TraefikServiceConfigBase)
-                and service.config.traefik
-            ):
-                service_opts = ResourceOptions(
-                    parent=ComponentResource(
-                        service.name(), service.name(), None, opts=self.child_opts
-                    )
-                )
-
-                TraefikServiceConfigBuilder(service.config.traefik).build_resources(
-                    opts=service_opts,
-                    traefik_service=traefik_service,
-                    extractor_args=service.extractor_args,
-                )
+            self.build_one(traefik_service, service)
 
         self.register_outputs({})
+
+    def build_one(
+        self, traefik_service: TraefikService, service: ServiceResourceBase
+    ) -> None:
+        if (
+            (
+                service.name() not in traefik_service.routers
+                and service.name() not in traefik_service.middlewares
+            )
+            and isinstance(service, ServiceWithConfigResourceBase)
+            and isinstance(service.config, TraefikServiceConfigBase)
+            and service.config.traefik
+        ):
+            service_opts = ResourceOptions(
+                parent=ComponentResource(
+                    service.name(), service.name(), None, opts=self.child_opts
+                )
+            )
+
+            TraefikServiceConfigBuilder(service.config.traefik).build_resources(
+                opts=service_opts,
+                traefik_service=traefik_service,
+                extractor_args=service.extractor_args,
+            )
