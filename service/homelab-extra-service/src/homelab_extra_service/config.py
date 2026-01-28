28d5950f12bfccb62ad2309c149bbe9677f23123
Vo Van Nghia <git@vnghia.com>
2026-01-28 21:35:14 +0100

    feat(service/extra): allow extra field in extra config

--- service/homelab-extra-service/src/homelab_extra_service/config.py
+++ service/homelab-extra-service/src/homelab_extra_service/config.py
@@ -1,6 +1,7 @@
 from homelab_dagu_config import DaguServiceConfigBase
 from homelab_traefik_config import TraefikServiceConfigBase
+from pydantic import ConfigDict
 
 
 class ExtraConfig(TraefikServiceConfigBase, DaguServiceConfigBase):
-    pass
+    model_config = ConfigDict(extra="allow")
