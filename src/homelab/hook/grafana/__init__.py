394e912027382e4dd1644fc082cbc98692a4fdac
Vo Van Nghia <git@vnghia.com>
2026-04-16 14:02:15 +0200

    feat(service/grafana): provision folder

--- src/homelab/hook/grafana/__init__.py
+++ src/homelab/hook/grafana/__init__.py
@@ -1,7 +1,38 @@
+import dataclasses
+import os
+from pathlib import Path
+
 import pulumiverse_grafana as grafana
 from homelab_extra_service import ExtraService
 from homelab_extra_service.config import ExtraConfig
 from pulumi import ResourceOptions
+
+
+@dataclasses.dataclass
+class Folder:
+    path: Path | None
+    resource: grafana.oss.Folder | None
+
+    def provision_subfolder(
+        self, grafana_folder: Path, grafana_opts: ResourceOptions
+    ) -> None:
+        opts = ResourceOptions.merge(
+            grafana_opts, ResourceOptions(parent=self.resource or None)
+        )
+
+        path = grafana_folder / self.path if self.path else grafana_folder
+        for folder in os.listdir(path):
+            subpath = self.path / folder if self.path else Path(folder)
+            resource = grafana.oss.Folder(
+                folder,
+                opts=opts,
+                parent_folder_uid=self.resource.uid if self.resource else None,
+                title=folder.capitalize(),
+                uid=subpath.as_posix().replace("/", "-"),
+            )
+            Folder(path=subpath, resource=resource).provision_subfolder(
+                grafana_folder, grafana_opts
+            )
 
 
 def pre_build(service: ExtraService[ExtraConfig]) -> None:
@@ -22,3 +53,8 @@ def post_build(service: ExtraService[ExtraConfig]) -> None:
             }
         ),
     )
+
+    grafana_folder = (
+        Path(__file__).parent.parent.parent.parent.parent / "config" / "grafana"
+    ).resolve(True)
+    Folder(path=None, resource=None).provision_subfolder(grafana_folder, grafana_opts)
