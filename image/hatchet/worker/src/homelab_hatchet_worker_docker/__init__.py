b96ae3d21a93ff7a58e2effa0fb43103e0f5e651
Vo Van Nghia <git@vnghia.com>
2026-05-05 17:52:09 +0200

    feat(service/hatchet): add dynamic load/unload module for worker

--- image/hatchet/worker/src/homelab_hatchet_worker_docker/__init__.py
+++ image/hatchet/worker/src/homelab_hatchet_worker_docker/__init__.py
@@ -1,7 +1,15 @@
+import dataclasses
+import importlib
 import logging
 import sys
+import threading
+import types
+from pathlib import Path
+from typing import Any, ClassVar, Self
 
-from hatchet_sdk import ClientConfig, Context, EmptyModel, Hatchet, Worker
+import watchfiles
+from hatchet_sdk import ClientConfig, Hatchet, Worker
+from hatchet_sdk.runnables.workflow import BaseWorkflow
 from homelab_hatchet_tool.config import Config
 
 config = Config.load()
@@ -14,18 +22,98 @@ logger = logging.getLogger()
 hatchet = Hatchet(config=ClientConfig(debug=config.debug, logger=logger))
 
 
-@hatchet.task()
-async def dummy(input: EmptyModel, ctx: Context) -> dict[str, str]:
-    return {}
+@dataclasses.dataclass
+class ModuleWorkflow:
+    instances: ClassVar[dict[Path, Self]] = {}
+
+    module: types.ModuleType
+    workflows: dict[str, BaseWorkflow[Any]]
+
+    @classmethod
+    def delete_workflow(cls, hatchet: Hatchet, name: str) -> None:
+        for workflow in hatchet.workflows.list(name).rows or []:
+            id = workflow.metadata.id
+            logger.info("Deleting workflow {}:{}".format(workflow.name, id))
+            hatchet.workflows.delete(id)
+
+    @classmethod
+    def load(cls, hatchet: Hatchet, worker: Worker, path: Path | str) -> Self:
+        path = Path(path).resolve(True)
+        logger.info("Loading module from {}".format(path))
+
+        if path in cls.instances:
+            instance = cls.instances[path]
+            module = importlib.reload(instance.module)
+            old_workflows = instance.workflows
+        else:
+            module = importlib.import_module(path.stem)
+            old_workflows = {}
+
+        workflows = {}
+        for workflow in module.build_workflows(hatchet):
+            if not isinstance(workflow, BaseWorkflow):
+                raise ValueError("Object {} is not a valid workflow".format(workflow))
+            logger.info("Registering workflow {}:{}".format(path.stem, workflow.name))
+            worker.register_workflow(workflow)
+            workflows[workflow.name] = workflow
+
+        for name in old_workflows:
+            if name not in workflows:
+                cls.delete_workflow(hatchet, name)
+
+        instance = cls(module=module, workflows=workflows)
+        cls.instances[path] = instance
+        return instance
+
+    @classmethod
+    def unload(cls, hatchet: Hatchet, path: Path | str) -> None:
+        path = Path(path)
+        logger.info("Unloading module from {}".format(path))
+
+        instance = cls.instances.pop(path)
+        for name in instance.workflows:
+            cls.delete_workflow(hatchet, name)
+
+        importlib.invalidate_caches()
 
 
-def load_workflows(hatchet: Hatchet, worker: Worker, config: Config) -> None:
-    for path in config.workflow_dir.glob("*.py"):
-        logger.info("Loading workflow from %s", path)
+@dataclasses.dataclass
+class WorkflowLoader:
+    hatchet: Hatchet
+    worker: Worker
+    config: Config
+    modules: dict[Path, ModuleWorkflow] = dataclasses.field(default_factory=dict)
+
+    def __post_init__(self) -> None:
+        sys.path.append(config.workflow_dir.as_posix())
+        for path in config.workflow_dir.glob("*.py"):
+            ModuleWorkflow.load(self.hatchet, self.worker, path)
+
+    def watch(self) -> None:
+        def _watch() -> None:
+            workflow_dir = self.config.workflow_dir
+            logger.info("Watching for modules in {}".format(workflow_dir))
+
+            for changes in watchfiles.watch(
+                workflow_dir,
+                watch_filter=lambda _, path: path.endswith(".py"),
+                raise_interrupt=False,
+                recursive=False,
+            ):
+                for change, file in changes:
+                    match change:
+                        case watchfiles.Change.added | watchfiles.Change.modified:
+                            ModuleWorkflow.load(self.hatchet, self.worker, file)
+                        case watchfiles.Change.deleted:
+                            path = Path(file)
+                            if not path.exists():
+                                ModuleWorkflow.unload(self.hatchet, path)
+
+        threading.Thread(target=_watch, daemon=True).start()
 
 
 def main() -> None:
     logger.info(config)
-    worker = hatchet.worker("worker", workflows=[dummy])
-    load_workflows(hatchet, worker, config)
+    worker = hatchet.worker("worker")
+    WorkflowLoader(hatchet, worker, config).watch()
     worker.start()
