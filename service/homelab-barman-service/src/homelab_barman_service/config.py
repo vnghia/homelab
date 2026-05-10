23bd96d5ca1612c9704b07b3fb02c8dfb480574b
Vo Van Nghia <git@vnghia.com>
2026-05-10 15:40:29 +0200

    feat(service/barman): migrate container cron job to hatchet task

--- service/homelab-barman-service/src/homelab_barman_service/config.py
+++ service/homelab-barman-service/src/homelab_barman_service/config.py
@@ -3,8 +3,13 @@ from homelab_docker.model.service.database.postgres.backup import (
     ServiceDatabasePostgresBackupConfigModel,
 )
 from homelab_extract import GlobalExtract
+from homelab_hatchet_config import HatchetServiceConfigBase
 
 
-class BarmanConfig(ServiceDatabasePostgresBackupConfigModel, DaguServiceConfigBase):
+class BarmanConfig(
+    ServiceDatabasePostgresBackupConfigModel,
+    DaguServiceConfigBase,
+    HatchetServiceConfigBase,
+):
     config_dir: GlobalExtract
     staging_dir: GlobalExtract
