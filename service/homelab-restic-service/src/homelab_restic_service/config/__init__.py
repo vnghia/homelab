2ebb4163e382147f44450cdc07e6a94bbf270dcc
Vo Van Nghia <git@vnghia.com>
2026-05-11 23:43:59 +0200

    feat(service/restic): add hatchet docker model

--- service/homelab-restic-service/src/homelab_restic_service/config/__init__.py
+++ service/homelab-restic-service/src/homelab_restic_service/config/__init__.py
@@ -1,8 +1,9 @@
 from homelab_dagu_config import DaguServiceConfigBase
 from homelab_extract import GlobalExtract
+from homelab_hatchet_config import HatchetServiceConfigBase
 
 
-class ResticConfig(DaguServiceConfigBase):
+class ResticConfig(DaguServiceConfigBase, HatchetServiceConfigBase):
     image: str
     configuration_dir: GlobalExtract
     cache_dir: GlobalExtract
