32058f4f0e0f63c7a083479bbad1144ca3dffe59
Vo Van Nghia <git@vnghia.com>
2026-05-16 12:10:52 +0200

    chore(service/balite): cleanup dagu

--- service/homelab-balite-service/src/homelab_balite_service/config.py
+++ service/homelab-balite-service/src/homelab_balite_service/config.py
@@ -1,6 +1,5 @@
-from homelab_dagu_config import DaguServiceConfigBase
 from homelab_hatchet_config import HatchetServiceConfigBase
 
 
-class BaliteConfig(DaguServiceConfigBase, HatchetServiceConfigBase):
+class BaliteConfig(HatchetServiceConfigBase):
     pass
